fee967725782a56222e87af7e349b8c53afef6a7
Yeabsra Ashebir <141928801+yeabnoah@users.noreply.github.com>
2024-04-11 04:17:38 +0300

    Update server.js
    cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+app.use(cors());
 const userRouter = require("./routes/users");
 // const postRouter = require("./routes/Posts");
-const cors = require("cors");
+
 
 // app.use(express.static("public"));
 app.use("/users", userRouter);
-app.use(cors());
+
 // app.use("/posts", postRouter);
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
